c8e6635a9bf4a401ef3dd4b624ece0863e45ae99
Brody14 <stefania.spinas@gmail.com>
2023-03-21 14:58:30 +0100

    FEAT form html + style

--- index.php
+++ index.php
@@ -9,8 +9,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 <body>
+
+    <section>
+        <div class="container text-center py-4">
+            <h1>Strong Password Generator</h1>
+        </div>
+        <div class="container py-5 d-flex justify-content-center">
+            <form method="GET">
+                <div class="row mb-3">
+                    <label  class="col-auto col-form-label">Lunghezza Password:</label>
+                    <div class="col-auto">
+                    <input type="number" class="form-control" name="length" >
+                    </div>
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Invia</button>
+            </form>
+        </div>
+    </section>
     
 </body>
 </html>
